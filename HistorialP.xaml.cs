d529446e51d685fe034823f9dacdbf105b779e7f
angel fabricio <ronoraangel09@gmail.com>
2018-11-25 16:03:43 -0600

    subir c

---
+++ HistorialP.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CanguroR
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class HistorialP : ContentPage
+	{
+		public HistorialP ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void irElemeto(object sender, PropertyChangingEventArgs e)
+        {
+           await Navigation.PushModalAsync(new ServicioHistorial());
+        }
+    }
+}
---
+++ ServicioHistorial.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CanguroR
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ServicioHistorial : ContentPage
+	{
+		public ServicioHistorial ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void Regresar(object sender, EventArgs e)
+        {
+           await Navigation.PopModalAsync();
+        }
+    }
+}
