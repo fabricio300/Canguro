e8598a42bf67d474c703732ad97564780a3a7929
angel fabricio <ronoraangel09@gmail.com>
2018-11-25 16:30:21 -0600

    mi primer commit

---
+++ DatosMandado.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CanguroR
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class DatosMandado : ContentPage
+	{
+		public DatosMandado ()
+		{
+			InitializeComponent ();
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+           // entregaInmediata.OnChanged += entregaInmediata_OnChangedClick;
+        }
+
+        private void entregaInmediata_OnChangedClick(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+            {
+                DisplayAlert("elección","entregar inmediatamente", "Ok");
+            }
+            else
+            {
+                DisplayAlert("elección", "entregar con tiempo", "Ok");
+            }
+        }
+
+        private async void IrFechaHora(object sender, EventArgs e)
+        {
+            var opcion = await DisplayAlert("Tarifar por servicio", "50$", "Aceptar", "Cancelar");
+           
+
+            if (opcion)
+            {
+                //Aceptar
+                await Navigation.PopModalAsync();
+            }
+            else
+            {
+                //Cancelar
+                await Navigation.PopModalAsync();
+            }
+        }
+
+        
+
+
+        private void seleccion(object sender, ToggledEventArgs e)
+        {
+            if (e.Value) {
+                FechaHara.IsVisible = false;
+                FechaHara.HeightRequest = 0;
+                //DisplayAlert("elección", "entregar inmediatamente", "Ok");
+            }
+            else
+            {
+                //DisplayAlert("elección", "entregar con tiempo", "Ok");
+                FechaHara.IsVisible = true;
+                FechaHara.HeightRequest = 190;
+            }
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace CanguroR
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        }
+
+        private async void IrASigiente(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new DatosMandado());
+            
+        }
+
+        private async void irHistorial(object sender, EventArgs e)
+        {
+           await Navigation.PushModalAsync(new HistorialP());
+        }
+    }
+}
